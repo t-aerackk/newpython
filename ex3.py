7291d1b481f68e370fa1a0be0269224f63626ac0
t-aerackk <aenjaal@mail.com>
2023-06-03 21:47:54 +0545

    Exercise 3rd

---
+++ ex3.py
@@ -0,0 +1,43 @@
+# #Area of circle
+# R=float(input("Enter radius of circle:\n"))
+# Area=3.14*(R*R)
+# print(f"The Area nof circle is {Area}")
+
+# #Create a variable, assign its value to another and square it 
+# A=5
+# B=A**2
+# print(B)
+
+# #creating a variable number, assigna a value and incremant it by 1 and multiply by 2 and print he final value
+# number=5
+# number +=1
+# print(number)
+# number=2*number
+# print(number)
+
+# #Swap three values using temp and without temp
+# a=5
+# b=6
+# c=9
+
+# old_a=a
+# temp=a
+# a=b
+# print(a)
+# temp=b
+# b=c
+# print(b)
+# temp=c
+# c=old_a
+# print(c)
+
+#swap three values without using temp entered by user
+# A=20
+# B=45
+# C=67
+# A= (input("Enter A value you want to swap:\n"))
+# B= (input("Enter B value you want to swap:\n"))
+# C= (input("Enter C value you want to swap:\n"))
+
+# A,B,C=B,C,A
+# print(f"The swapped values of {A}, {B}, {C} is {B}, {C}, {A}")
