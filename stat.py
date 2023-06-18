9f22e1ed2d3f3654c049b4b29464aa68d7525662
t-aerackk <aenjaal@mail.com>
2023-06-18 21:26:58 +0545

    6/19

---
+++ stat.py
@@ -0,0 +1,13 @@
+import statistics as stat
+# mean
+
+print(stat.mean([10,12,15,120,13,25]))
+
+#median
+print(stat.median([10,12,15,120,13,35]))
+
+#mode
+print(stat.mode([10,12,15,120,13,35]))
+
+#SD
+print(stat.stdev([10,12,15,120,13,35]))
