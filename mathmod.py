8bb36549a39f1e1d50f0c5e1ac63eb66dcde161e
t-aerackk <aenjaal@mail.com>
2023-06-22 21:11:55 +0545

    6/21

---
+++ mathmod.py
@@ -0,0 +1,28 @@
+#Math modules
+#1.Sqrt
+import math
+print(math.sqrt(64))
+
+#2. Ceiling function
+print(math.ceil(3.5))
+
+#3. Floor functon
+print(math.floor(3.5))
+print(math.floor(4.5))
+
+#4. Power calculate
+print(math.pow(3,5))
+
+#5. Absolute value
+print(math.fabs(-3.5))
+print(math.fabs(-33.00065))
+
+#6. Factorial
+print(math.factorial(9))
+print(math.factorial(8))
+
+#7. Triginometry
+print(math.sin(90))
+print(math.cos(90))
+print(math.tan(90))
+
